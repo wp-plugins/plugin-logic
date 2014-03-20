30cae4912327ecd60927c7f5c1e47e4da37db323
simon_h <simon_h@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-20 17:30:13 +0000

    Add tag versions
    git-svn-id: https://plugins.svn.wordpress.org/plugin-logic/tags/1.0.1@878916 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ plugin-logic-table.php
@@ -0,0 +1,246 @@
+<?php	
+/***
+ * Create the html-table for the dashboard view
+ *
+ * @package	    Plugin Logic
+ * @author      simon_h
+ * @since       1.0.0
+ */
+ 
+ // Security check
+ if ( ! class_exists('WP') ) {
+	header('Status: 403 Forbidden');
+	header('HTTP/1.1 403 Forbidden');
+	exit();
+}
+
+if ( ! class_exists( 'plulo_table' ) ) {
+	
+	class plulo_table {
+
+		protected static $classobj = NULL;
+		public $html_output = '';
+		public $plugin_base = '';
+		public $all_on_dash = false;
+		
+		
+		/***
+		 * Handler for the action 'init'. Instantiates this class.
+		 * @since 1.0.0
+		 */
+		public static function init() {
+			NULL === self::$classobj and self::$classobj = new self();
+			return self::$classobj;
+		}
+		
+	
+		 /***
+		  * Install settings;
+		  * @since 1.0.0
+		  */
+		public function __construct( $plugin_basename = '', $all_on_dash = false ) {
+			$this->plugin_base = $plugin_basename;
+			$this->all_on_dash = $all_on_dash;
+			$this->html_output = $this->create_the_table(); 
+		}
+		
+		
+		/***
+		 * Get colors from the dashboard style
+		 * @since 1.0.0
+		 */
+		public function get_adminbar_colors(){
+			?>
+			<script type="text/javascript">
+
+				function getStyle(el, cssprop){
+					if (el.currentStyle) //IE
+						return el.currentStyle[cssprop]
+					else if (document.defaultView && document.defaultView.getComputedStyle) //Firefox
+						return document.defaultView.getComputedStyle(el, "")[cssprop]
+					else //try and get inline style
+						return el.style[cssprop]
+				}
+
+				function rgbStringToHex(rgbStr){
+					var a = rgbStr.split("(")[1].split(")")[0];
+					a = a.split(",");
+					var b = a.map(function(x){             //For each array element
+						x = parseInt(x).toString(16);      //Convert to a base16 string
+						return (x.length==1) ? "0"+x : x;  //Add zero if we get only one character
+					})
+					return "#"+b.join("");
+				}	
+						
+				var wpadminbar = document.getElementById("wpadminbar");
+				var table = document.getElementById("hrow");
+				
+				// Paint the table headline with admin colors 
+				if ( wpadminbar ) {
+					admin_background = rgbStringToHex( getStyle(wpadminbar, 'backgroundColor') );
+					admin_color = rgbStringToHex( getStyle(wpadminbar, 'color') );
+				
+					if ( (admin_background == '#222') || (admin_background == '#222222') ) {
+						table.style.background = '#222';
+						table.style.color = '#EEE';
+					} else {
+						table.style.background = admin_background;
+						table.style.color = admin_color;
+					}
+					
+				} else {
+					table.style.background = '#222';
+					table.style.color = '#f1f1f1';
+				}
+		
+			</script> 
+			<?php
+		}
+				
+
+		/***
+		 * Table CSS-Style
+		 * @since 1.0.0
+		 */
+		public function create_style() { 
+			return '<!-- Table Style -->
+					<style type="text/css"> 
+					.tftable { 
+						font-size:12px; 
+						color:#333333; 
+						border-width:1px; 
+						border-color:#729ea5; 
+						border-collapse:collapse; 
+						width:100%;
+					}
+					.tftable th {	
+						border-width:1px; 
+						padding:8px; 
+						border-style:solid; 
+						border-color:#EFEFEF; 
+						text-align:left;
+					}
+					.tftable tr { background-color:#fff; color:#000; }
+					.tftable td { font-size:12px; border-width:1px; padding:8px; border-style:solid; border-color:#EFEFEF; }
+					</style>' . "\n";
+		}
+	
+	
+		/***
+		 * Table HTML-Output
+		 * @since 1.0.0
+		 */
+		public function create_the_table() { 
+			global $wpdb;
+			$table = $wpdb->base_prefix . 'plugin_logic';
+			$structur = '';
+			( get_option( 'plulo_on_dash_col' ) !== false ) ? $on_dash_columm = get_option( 'plulo_on_dash_col' ) : $on_dash_columm = '';
+	
+			if(!function_exists('get_plugins')) {
+				require_once (ABSPATH . 'wp-admin/includes/plugin.php');
+			}
+			
+			$plugin_infos = array();
+			if ( is_admin() ) {
+				$plugin_infos = get_plugins();
+			}
+			
+			$plugins_list = get_option( 'active_plugins', array () );
+			
+			// Load data from db
+			$db_pl_list = array();
+			if ( $wpdb->get_var( "SHOW TABLE STATUS LIKE '$table'") ) {
+				$db_pl_list = $wpdb->get_results( "SELECT name, on_dashboard, logic, urls, words FROM $table ORDER BY name ASC" );
+			} 
+			
+			// Filter inactive plugins with rules and add it to the $plugins_list
+			$no_dashboard_plugs = array();
+			foreach($db_pl_list as $db_pl) {
+				if ( ! in_array($db_pl->name, $plugins_list ) ) {
+					$no_dashboard_plugs[] = $db_pl->name;
+				}
+			}
+			if ( count($no_dashboard_plugs ) > 0 ) {
+				$plugins_list = array_merge($plugins_list, $no_dashboard_plugs);
+				sort($plugins_list);
+			}	
+			
+			// Check if relevant Plugins available
+			if ( count($plugins_list) == 1 ) {
+				add_action('admin_footer', function () {
+					?>
+					<script type="text/javascript">
+						var tableFoot = document.getElementById("tfoot");
+						tableFoot.style.display = 'none';
+					</script> 
+					<?php
+					}
+				);
+				$structur .= "<div class=\"update-nag\">\n"; 
+				$structur .= "	<h4> There are no active Plugins or inactive Plugins with Rules. </h4>\n"; 
+				$structur .= "</div>\n"; 
+				return $structur;
+			}
+			
+			// Create the table
+			add_action('admin_footer',array($this, 'get_adminbar_colors') ) ;
+			$structur.= $this->create_style(); 
+			$structur.= '<table class="tftable" border="1">'."\n"; 
+			$structur.= "<tr id=\"hrow\">\n";
+			$structur.= "	<th >Activated Plugins</th>\n";
+			if ($on_dash_columm == 'checked') $structur.= "	<th>Behavior on Dashbord</th>\n";
+			$structur.= "	<th>Active / Inactive</th>\n";
+			$structur.= "	<th>Urls or occurring Words</th>\n";
+			$structur.= "</tr>\n";
+			
+			$z = 0;		
+			foreach ($plugins_list as $p) {
+				if ($p == $this->plugin_base ) continue;
+				$on_dashboard = 1;
+				$logic = 0;
+				$act_rules = array();
+				$act_rules_str = '';
+				
+				// Check if there are rules for the Plugin in the Database
+				foreach($db_pl_list as $db_pl) {
+					if ($p == $db_pl->name) {
+						$act_rules = array_merge( unserialize($db_pl->urls), unserialize($db_pl->words) );
+						$act_rules_str = implode(",\n", $act_rules);
+						$logic = $db_pl->logic;
+						$on_dashboard = $db_pl->on_dashboard;
+						break;
+					}
+				}
+				
+				$select_in = '';
+				$select_ex = '';
+				($logic == 0) ?  $select_in = 'checked' : $select_ex = 'checked';
+				$always_on = '';
+				($on_dashboard == 1) ?  $always_on = 'checked' : $always_on = '';
+				( !is_plugin_active($p) && !$this->all_on_dash ) ? $inactive = 'style="background:#D3D1D1;"' : $inactive = '';
+				($on_dash_columm == 'checked') ? $txt_in_style = 'style="width:70%;"' : $txt_in_style = 'style="width:78%;"'; 
+								
+				$structur.= "<tr $inactive> \n";
+				$structur.= "  <td style=\"min-width:98px;\">" . $plugin_infos[$p]['Name'] . "</td>\n";
+				if ($on_dash_columm == 'checked') {
+					$structur.= "  <td style=\"min-width:86px; width:86px;\"> \n";
+					$structur.= '	 	<input type="hidden" name="plcon_checklist['. $z .']" value="0">' . " \n";
+					$structur.= '		<input type="checkbox" name="plcon_checklist['. $z .']" value="1" '. $always_on .'>Always on' . "\n";
+					$structur.= "  </td> \n";
+				}
+				$structur.= "  <td style=\"min-width:95px;\"> \n";
+				$structur.= '	 	<input type="radio" name="plcon_radiolist['. $z .']" value="0" '. $select_in .'> Active on:' . "<br> \n";
+				$structur.= '		<input type="radio" name="plcon_radiolist['. $z .']" value="1" '. $select_ex .'> Inactive on:' . "\n";
+				$structur.= "  </td> \n";
+				$structur.= '  <td '. $txt_in_style .' ><textarea name="plcon_txt_list['. $z .']" style="width:100%; min-height:74px">'. $act_rules_str .'</textarea></td>' . "\n";
+				$structur.= "</tr> \n";
+
+				$z++;
+			}
+			$structur.= "</table> \n";
+			return $structur;
+		}
+			
+	} // end class
+	
+} // end if class exists
---
+++ readme.txt
@@ -0,0 +1,50 @@
+=== Plugin Logic ===
+Contributors: simon_h
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=SMX67F2CZLDZL
+Tags: deactivate plugins by url, activate plugins by url, deactivate plugins by rules, disable plugins by page, disable plugins by rules 
+Requires at least: 3.8.0
+Tested up to: 3.8.1
+Stable tag: 1.0.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Activate plugins on pages only if they are really needed.
+
+== Description ==
+
+There are many possibilities to increase the speed of your Wordpress page.  
+One of them, which is often forgotten, is to deactivate Plugins on pages, where they are not needed. 
+This Plugin allows you to do this on a very easy way. 
+So you can reduce the amount of JavaScript and CSS files which are loaded and SQL queries run at page load.
+
+== Installation ==
+
+1. Install the Plugin
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Create your own rules on the Plugin Logic options page
+
+== Frequently Asked Questions ==
+
+**Q. How do I format my rules?**
+
+Use "http://" or "https://" at the beginning to define urls. 
+Words without these keywords at the beginning are interpreted as normal words. They will be searched in the requested url.
+To separate your rules use the comma sign.
+
+**Q. Will this work on a multisite installation?**
+
+It has not been tested with a multisite installation yet.
+
+== Screenshots ==
+
+1. The Options Page.
+2. Options for the Behavior on Dashbord.
+
+== Changelog ==
+
+= 1.0.1 =
+* Fixed uninstall
+* Standard WP table charset and collate added 
+
+= 1.0.0 =
+* Initial release
